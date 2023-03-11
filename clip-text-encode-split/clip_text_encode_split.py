21298c266644446d7f44d7c48f4c55333d7597e9
m957ymj75urz <m957ymj75urz@gmail.com>
2023-03-11 14:14:49 +0100

    added rawtextcombine and rawtextreplace

--- clip-text-encode-split/clip_text_encode_split.py
+++ clip-text-encode-split/clip_text_encode_split.py
@@ -5,7 +5,7 @@ class RawText:
     
     RETURN_TYPES = ("RAW_TEXT",)
     FUNCTION = "forward"
-    CATEGORY = "conditioning"
+    CATEGORY = "utils"
 
     def forward(self, raw_text):
         return (raw_text, )
@@ -23,8 +23,45 @@ class RawTextCLIPEncode:
 
     def encode(self, clip, raw_text):
         return ([[clip.encode(raw_text), {}]], )
+    
+class RawTextCombine:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "raw_text_1": ("RAW_TEXT", ), 
+                "raw_text_2": ("RAW_TEXT", )
+            }
+        }
+    RETURN_TYPES = ("RAW_TEXT",)
+    FUNCTION = "combine"
+
+    CATEGORY = "utils"
+
+    def combine(self, raw_text_1, raw_text_2):
+        return (raw_text_1 + "," + raw_text_2, )
+    
+class RawTextReplace:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "search": ("STRING", {}),
+                "subject": ("RAW_TEXT", ), 
+                "replace": ("RAW_TEXT", )
+            }
+        }
+    RETURN_TYPES = ("RAW_TEXT",)
+    FUNCTION = "replace"
+
+    CATEGORY = "utils"
+
+    def replace(self, search, subject, replace):
+        return (subject.replace(search, replace), )
 
 NODE_CLASS_MAPPINGS = {
     "RawText": RawText,
-    "RawTextEncode": RawTextCLIPEncode
+    "RawTextEncode": RawTextCLIPEncode,
+    "RawTextCombine": RawTextCombine,
+    "RawTextReplace": RawTextReplace
 }
